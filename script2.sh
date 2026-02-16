b340425bd0d138932e9db5ad89bbb3a659b20046
Ahmed Mahmoud1P <109467491+AhemdMahmoud@users.noreply.github.com>
2026-02-16 13:40:05 +0200

    jsdgsjd

--- script2.sh
+++ script2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install cowsay -y
 cowsay -f dragon "Run for Cover, I am a Dragon .... RaWR" > $GITHUB_WORKSPACE/dragon.txt
 cat $GITHUB_WORKSPACE/dragon.txt
 grep -i "dragon" $GITHUB_WORKSPACE/dragon.txt
