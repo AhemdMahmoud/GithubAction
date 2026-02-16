5e0ef2aac2b08b04d9ff9f611be58d4904aef2f2
Ahmed Mahmoud1P <109467491+AhemdMahmoud@users.noreply.github.com>
2026-02-16 13:24:54 +0200

    thraid

--- script2.sh
+++ script2.sh
@@ -1,5 +1,12 @@
-#! /bin/bash
-sudo apt-get install cowsay -y 
-cowsay -f dragon "Run for Cover, iam a Dragon .... RaWR">> dragon.txt
+#!/bin/bash
+# Install cowsay
+sudo apt-get update && sudo apt-get install cowsay -y
+
+# Generate ASCII art
+/usr/games/cowsay -f dragon "Run for Cover, I am a Dragon .... RaWR" > dragon.txt
+
+# Display content
 cat dragon.txt
+
+# Check for keyword
 grep -i "dragon" dragon.txt
