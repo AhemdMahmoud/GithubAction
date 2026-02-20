128e697ebe7394af883ad826214cee8b0d2670eb
Ahmed Mahmoud1P <109467491+AhemdMahmoud@users.noreply.github.com>
2026-02-20 15:50:24 +0200

    v10

--- etl.py
+++ etl.py
@@ -3,6 +3,7 @@ import csv
 data = [
     {"id": 1, "name": "Ali", "age": 25},
     {"id": 2, "name": "Sara", "age": 30},
+    {"id": 3, "name": "Ahmed", "age": 24},
 ]
 
 with open("dbt_project/seeds/output_csv.csv", "w", newline="") as f:
